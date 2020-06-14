7e91b9aab46e56589fbd583dd0e34b8a817d9f82
nisha <nishakumarx@gmail.com>
2020-06-14 09:46:29 -0700

    Add command line argument --version
    Signed-off-by: nisha <nishakumarx@gmail.com>

--- genv/genv.go
+++ genv/genv.go
@@ -7,12 +7,20 @@ package main
 import (
         "fmt"
         "os"
+        "flag"
         "github.com/nishakm/genv/pkg/script"
         "github.com/nishakm/genv/pkg/workspace"
 )
 
 func main() {
-    folder := os.Args[1]
+    flag.Usage = func() {
+        fmt.Fprintf(os.Stderr, "Usage: %s [options] folder\n", os.Args[0])
+        flag.PrintDefaults()
+    }
+    verPtr := flag.String("version", "", "Go version")
+    flag.Parse()
+    folder := flag.Arg(0)
+    fmt.Println("version", *verPtr)
     // create the workspace with the folder
     pathtoactivate := workspace.Create(folder)
     // get the location of the new gopath
